c2db653de955557669d558dcf50cfe4adb3dff35
Denis <orkvud@gmail.com>
2018-11-23 18:16:23 +0200

    Nav bar.

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,59 @@
 
 "use strict";
 
-let date = document.getElementById("date")
-date.value = "2018-01-02"
+let isNawMenuShown;
 
-let button = document.getElementById("button")
+let containerMenu = document.getElementById("containerMenu");
+console.log(containerMenu);
 
+let navMenu = document.getElementById("navMenu");
+navMenu.addEventListener("click", (evt) =>{
+   console.log(evt)
+});
+
+let date = document.getElementById("date");
+date.value = "2018-01-02";
+
+let button = document.getElementById("button");
 button.addEventListener("click", () => {
-    let d = date.value
-    console.log(d)
+    date.style.display = "block";
+    let d = date.value;
+    console.log(d);
     if (d) {
-        alert("Your date is " + date.value)
+        alert("Your date is " + date.value);
     } else {
-        alert("You didn't choose a date")
+        alert("You didn't choose a date");
+    };
+});
+
+document.getElementById("closeOpenBtn").addEventListener("click", () => {
+    if (isNawMenuShown) {
+        showNavMenu(false);
+    } else {
+        showNavMenu(true);
     }
-})
+});
+
+function showNavMenu(show) {
+    let containerMenu = document.getElementById("containerMenu");
+    let closeOpenBtn = document.getElementById("closeOpenBtn");
+    let navMenu = document.getElementById("navMenu");
+
+    console.log(containerMenu);
+    if (show){
+        containerMenu.style.width = "20vw";
+        closeOpenBtn.style.left = "20vw";
+        navMenu.style.opacity = "1.0"
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+        isNawMenuShown = true;
+    } else {
+        containerMenu.style.width = "0vw";
+        closeOpenBtn.style.left = "0vw";
+        navMenu.style.opacity = "0.0"
+        document.body.style.backgroundColor = "rgba(255, 211, 144, 0.39)";
+        isNawMenuShown = false;
+    }
+}
+
+showNavMenu(false);
+
